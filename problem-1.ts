5d2cd713bc6a467ce3a4fc862f5a0eb900ca18b1
Rasibul <rasibul179@gmail.com>
2024-11-07 23:14:18 +0600

    solve the first problem

--- problem-1.ts
+++ problem-1.ts
@@ -8,6 +8,6 @@
     }
 
     // Example usage
-    const result: number = sumArray([1, 2, 3, 4, 5]);
-    console.log(result); // Output: 15
+    const result: number = sumArray([1, 2, 3, 4, 5,6,7,8,9,10]);
+    console.log(result); 
 }
